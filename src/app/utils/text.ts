9cedb19373ad93fd1d7edbb0ecfd93c6740bd194
Adam-fromSZTE <84191652+Adam-fromSZTE@users.noreply.github.com>
2025-10-15 12:15:47 +0200

    feat: first version of header and nav

---
+++ src/app/utils/text.ts
@@ -0,0 +1,9 @@
+export const toCamelCase = (str: string) => {
+    return str
+        .toLowerCase()
+        .split(" ")
+        .map((word, index) =>
+            index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+        )
+        .join("");
+};
